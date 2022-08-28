9800c0e38cee07b7c6fd770316b365b659d19b0a
Ulises Armolla <uliarmolla@gmail.com>
2022-08-27 22:53:22 -0300

    se solucionaron problemas de estilos y se refactoriza codigo js

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,29 +1,43 @@
-const bars = document.querySelector('.buttonBars');
 const menu = document.querySelector('.menu');
-const links = document.querySelectorAll('.linkMenu');
-const logo = document.querySelector('.logo');
 
-bars.addEventListener('click', ()=>{
-    
-    if (menu.style.left !== '0%') {
-        menu.style.left = '0%';
-    } else {
-        menu.style.left = '-100%';
-    }
-
+function animateMenu (){
     document.querySelector('.topBar').classList.toggle('activeTopBar');
     document.querySelector('.middleBar').classList.toggle('activeMiddleBar');
     document.querySelector('.bottomBar').classList.toggle('activeBottomBar');
+}
+
+function showMenu (){
+    menu.style.left = '0%';
+    animateMenu();
+}
+
+function hideMenu (){
+    menu.style.left = '-100%';
+    animateMenu();
+}
+
+document.querySelector('.buttonBars').addEventListener('click', ()=>{
+    if (menu.style.left !== '0%') {
+        showMenu();
+    } else {
+        hideMenu();
+    }
 });
 
-links.forEach((value) => {
+document.querySelectorAll('.linkMenu').forEach((value) => {
     value.addEventListener('click', ()=>{
-        menu.style.left = '-100%';
+       hideMenu();
     });
 });
 
-logo.addEventListener('click', ()=>{
+document.querySelector('.logo').addEventListener('click', ()=>{
     if (menu.style.left !== '-100%') {
-        menu.style.left = '-100%'
-    }
+        hideMenu();
+    }  
 });
+
+window.addEventListener('resize', ()=>{
+    if (screen.width >= 992 && menu.style.left !== '-100%') {
+        hideMenu();
+    }
+})
